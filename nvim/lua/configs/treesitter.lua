0ee8e52f9ceaccdf7a321a450620ae741bfc8673
3DRX <kjy20030228@icloud.com>
2023-10-05 18:32:31 +0800

    update treesitter settings, add ensure install list and incremental selection

--- nvim/lua/configs/treesitter.lua
+++ nvim/lua/configs/treesitter.lua
@@ -1,28 +1,41 @@
--- Treesitter
 require 'nvim-treesitter.configs'.setup {
-    -- A list of parser names, or "all"
-    ensure_installed = {},
-    -- Install parsers synchronously (only applied to `ensure_installed`)
-    sync_install = false,
-    -- Automatically install missing parsers when entering buffer
-    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-    auto_install = true,
-    -- List of parsers to ignore installing (for "all")
-    ignore_install = {},
-    ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-    -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-    highlight = {
-        -- `false` will disable the whole extension
+    ensure_installed = {
+        "json",
+        "javascript",
+        "typescript",
+        "tsx",
+        "html",
+        "css",
+        "markdown",
+        "markdown_inline",
+        "vim",
+        "lua",
+        "gitignore",
+        "c",
+        "cpp",
+        "cmake",
+        "csv",
+        "ninja",
+        "rust",
+        "python",
+        "bash",
+    },
+    incremental_selection = {
         enable = true,
-        -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-        -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-        -- the name of the parser)
-        -- list of language that will be disabled
-        disable = {},
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
+        keymaps = {
+            init_selection = "<C-n>",
+            node_incremental = "<C-n>",
+            scope_incremental = false,
+            node_decremental = "<C-m>",
+        },
+    },
+    auto_install = true,
+    ignore_install = {},
+    highlight = {
+        enable = true,
         additional_vim_regex_highlighting = false,
+    },
+    indent = {
+        enable = true,
     },
 }
