c75eeb4235c4fa33871a67e1accfee61f1264970
3DRX <kjy20030228@icloud.com>
2023-10-05 12:03:39 +0800

    Update all plugins, update indentline config

--- nvim/lua/configs/indentline.lua
+++ nvim/lua/configs/indentline.lua
@@ -1,6 +1,5 @@
--- indent_blankline
-require("indent_blankline").setup {
-    -- for example, context is off by default, use this to turn it on
-    show_current_context = true,
-    show_current_context_start = false,
-}
+require("ibl").setup({
+    indent = {
+        char = "╎",
+    },
+})
