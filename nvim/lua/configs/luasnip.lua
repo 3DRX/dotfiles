654adf3635f57b4a2dd1ff8d4711b3223cd92042
3DRX <kjy20030228@icloud.com>
2024-03-08 09:31:20 +0800

    Fix luasnip keymap.

--- nvim/lua/configs/luasnip.lua
+++ nvim/lua/configs/luasnip.lua
@@ -1,14 +1,16 @@
 local ls = require("luasnip")
--- key map
-vim.keymap.set({ "i", "s" }, '<C-">', function()
-	if ls.expand_or_jumpable() then
-		ls.expand_or_jump()
+-- <Ctrl-Right> for jumping forward,
+-- <Ctrl-Left> for jumping backward,
+-- <Ctrl-Up> for changing the active choice.
+vim.keymap.set({ "i", "s" }, "<C-Right>", function()
+	ls.jump(1)
+end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<C-Left>", function()
+	ls.jump(-1)
+end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<C-Up>", function()
+	if ls.choice_active() then
+		ls.change_choice(1)
 	end
 end, { silent = true })
-vim.keymap.set({ "i", "s" }, "<C-;>", function()
-	if ls.jumpable(-1) then
-		ls.jump(-1)
-	end
-end, { silent = true })
-
-require("luasnip.loaders.from_vscode").lazy_load()
+-- require("luasnip.loaders.from_vscode").lazy_load()
