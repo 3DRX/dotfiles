01a796eb59b8fee2b8d2e7cbd9c1d20aa7c360dc
3DRX <3drxkjy@gmail.com>
2024-09-15 16:23:50 +0800

    Update nvim config

--- nvim/init.vim
+++ nvim/init.vim
@@ -41,13 +41,13 @@ runtime ./plugins/tex.vim
 lua require("init")
 
 
-" if exists("&termguicolors") && exists("&winblend")
-"     syntax enable
-"     set termguicolors
-"     set winblend=0
-"     set wildoptions=pum
-"     set pumblend=5
-"     set background=dark
-" endif
-" hi Normal ctermbg=256
-" hi clear LineNr
+if exists("&termguicolors") && exists("&winblend")
+    syntax enable
+    set termguicolors
+    set winblend=0
+    set wildoptions=pum
+    set pumblend=5
+    set background=dark
+endif
+hi Normal ctermbg=256
+hi clear LineNr
--- nvim/lua/configs/colorscheme.lua
+++ nvim/lua/configs/colorscheme.lua
@@ -1,36 +1,38 @@
--- require("kanagawa").setup({
--- 	compile = false,
--- 	undercurl = true,
--- 	commentStyle = { italic = true },
--- 	functionStyle = {},
--- 	keywordStyle = { italic = true },
--- 	statementStyle = { bold = true },
--- 	typeStyle = {},
--- 	transparent = true,
--- 	dimInactive = false,
--- 	terminalColors = true,
--- 	colors = {
--- 		palette = {},
--- 		theme = {
--- 			wave = {
--- 				ui = {
--- 					bg = "none",
--- 					float = {
--- 						bg = "black",
--- 					},
--- 				},
--- 			},
--- 			lotus = {},
--- 			dragon = {},
--- 			all = {},
--- 		},
--- 	},
--- 	theme = "wave",
--- 	background = {
--- 		dark = "wave",
--- 		light = "lotus",
--- 	},
--- })
+require("kanagawa").setup({
+	compile = false,
+	undercurl = true,
+	commentStyle = { italic = true },
+	functionStyle = {},
+	keywordStyle = { italic = true },
+	statementStyle = { bold = true },
+	typeStyle = {},
+	transparent = true,
+	dimInactive = false,
+	terminalColors = true,
+	colors = {
+		palette = {},
+		theme = {
+			wave = {
+				ui = {
+					bg = "none",
+					float = {
+						bg = "black",
+					},
+				},
+			},
+			lotus = {},
+			dragon = {},
+			all = {},
+		},
+	},
+	theme = "wave",
+	background = {
+		dark = "wave",
+		light = "lotus",
+	},
+})
+vim.cmd("set background=dark")
+vim.cmd("colorscheme kanagawa")
 
-vim.cmd("set background=light")
-vim.cmd("colorscheme zenbones")
+-- vim.cmd("set background=light")
+-- vim.cmd("colorscheme zenbones")
