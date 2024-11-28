01506cff1b463478e27e96fad9c92cbca2a915fb
3DRX <3drxkjy@gmail.com>
2024-11-28 16:05:54 +0800

    Add keymap gD

--- nvim/lua/configs/lsp.lua
+++ nvim/lua/configs/lsp.lua
@@ -211,6 +211,7 @@ vim.keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)
 vim.keymap.set("n", "<leader>ic", "<cmd>Lspsaga incoming_calls<CR>", opts)
 vim.keymap.set("n", "<leader>oc", "<cmd>Lspsaga outgoing_calls<CR>", opts)
 vim.keymap.set("n", "<leader>ot", "<cmd>Lspsaga outline<CR>", opts)
+vim.api.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
 
 vim.api.nvim_create_user_command("FF", function()
 	vim.lsp.buf.format()
